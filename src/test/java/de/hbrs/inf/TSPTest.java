f370388de6af93c2ed268a5810e7088850f7e110
Hendrik Klohn <Hendrik.Klohn@cybercon.de>
2018-06-27 13:54:41 +0200

    Change testGetTSPFromJson, cause new file locations and add test for all json files in 'tsplib' directory

--- src/test/java/de/hbrs/inf/TSPTest.java
+++ src/test/java/de/hbrs/inf/TSPTest.java
@@ -3,21 +3,37 @@ package de.hbrs.inf;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.logging.Logger;
+
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class TSPTest{
 
+	private static final Logger log = Logger.getLogger( TSPTest.class.getName() );
+
 	@Test
 	public void testGetTSPFromJson() {
-		assertNull( TSP.getObjectFromJson( "resources/tsp_test2.json" ) );
-		assertNotNull( TSP.getObjectFromJson( "resources/tsp_test.json" ) );
+		assertNull( TSP.getObjectFromJson( "resources/test/tsp_test2.json" ) );
+		assertNotNull( TSP.getObjectFromJson( "resources/test/tsp_test.json" ) );
 	}
 
 	@Test
 	public void testGrb() {
-		TSP tsp = TSP.getObjectFromJson( "resources/gr137.json" );
+		TSP tsp = TSP.getObjectFromJson( "resources/tsplib/a280.json" );
 		tsp.grbOptimize();
+	}
+
+	@Test
+	public void testExamplesTspLib() {
+		File directory = new File( "resources/tsplib" );
+		for( File file : directory.listFiles() ) {
+			log.info( "##################### " + file.getName() + "#####################" );
+			TSP tsp = TSP.getObjectFromJson( file.getPath() );
+			tsp.grbOptimize();
+		}
+
 	}
 
 }
