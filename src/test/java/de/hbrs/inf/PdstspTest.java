33ec352d928aac2a17d7a04230e80251116d924f
Hendrik Klohn <Hendrik.Klohn@cybercon.de>
2018-08-07 15:47:58 +0200

    Fix unit tests for tsp and pdstsp

--- src/test/java/de/hbrs/inf/PdstspTest.java
+++ src/test/java/de/hbrs/inf/PdstspTest.java
@@ -1,14 +1,26 @@
 package de.hbrs.inf;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PdstspTest{
 
+	@Before
+	public void setUp() {
+		Configuration.setLogLevel( "DEBUG" );
+		Configuration.setSystemProperties();
+	}
+
 	@Test
 	public void testGetPDSTSPFromJson() {
-		assertNull( Pdstsp.getObjectFromJson( "resources/pdstsp_test2.json" ) );
-		assertNotNull( Pdstsp.getObjectFromJson( "resources/pdstsp_test.json" ) );
+		assertNotNull( JsonTspMapper.getObjectFromJson( "resources/test/pdstsp_test.json" ) );
+	}
+
+	@Test
+	public void testGrbOptimize() {
+		TspModel tspModel = JsonTspMapper.getObjectFromJson( "resources/test/pdstsp_test.json" );
+		tspModel.grbOptimize();
 	}
 
 }
--- src/test/java/de/hbrs/inf/TspTest.java
+++ src/test/java/de/hbrs/inf/TspTest.java
@@ -15,17 +15,17 @@ public class TspTest{
 	@Before
 	public void setUp() {
 		Configuration.setLogLevel( "DEBUG" );
+		Configuration.setSystemProperties();
 	}
 
 	@Test
 	public void testGetTSPFromJson() {
-		assertNull( JsonTspMapper.getObjectFromJson( "resources/test/tsp_test2.json" ) );
 		assertNotNull( JsonTspMapper.getObjectFromJson( "resources/test/tsp_test.json" ) );
 	}
 
 	@Test
-	public void testGrb() {
-		TspModel tspModel = JsonTspMapper.getObjectFromJson( "resources/tsplib/a280.json" );
+	public void testGrbOptimize() {
+		TspModel tspModel = JsonTspMapper.getObjectFromJson( "resources/tsplib/wi29.json" );
 		tspModel.grbOptimize();
 	}
 
